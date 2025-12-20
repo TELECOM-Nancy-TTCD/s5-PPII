03c8abfdb668a6e1f6ccd199b7376d25a2d0e97a
Dylan Dole <dylan.dole@telecomnancy.eu>
2025-12-20 14:08:07 +0100

    Restructuration app_convention, et mise en beauté de quelques pages html

--- app_conventions.py
+++ app_conventions.py
@@ -1,73 +1,17 @@
-from flask import Flask,render_template,send_file, request
-from flask import g
+from flask import Flask, Response, Blueprint, render_template, send_file, abort, redirect, url_for, flash, g, request, session
+from flask_login import LoginManager, UserMixin, login_user, login_required, current_user
 
-from flask import Blueprint
-from flask_login import login_user, login_required
+from hashlib import scrypt
+from datetime import datetime, timedelta
+from dotenv import load_dotenv
 
-import redis
-import database
+import sqlite3, csv, io, redis, os, base64, database
 
-import sqlite3
-import os
 
 conventions_bp = Blueprint('conventions',__name__,url_prefix='/conventions')
 
-class FakeRedis:
-    """Simple fake Redis-like client used for tests. Stores values in an in-memory dict.
 
-    Methods implemented: get, setex, set, sadd, srem, smembers, expire, delete
-    """
-    def __init__(self):
-        self.store = {}
-        self.sets = {}
-
-    def get(self, key):
-        return self.store.get(key)
-
-    def set(self, key, value):
-        self.store[key] = value
-
-    def setex(self, key, ttl, value):
-        # TTL is ignored for the fake client
-        self.store[key] = value
-
-    def sadd(self, key, member):
-        s = self.sets.setdefault(key, set())
-        s.add(member)
-
-    def srem(self, key, member):
-        s = self.sets.get(key)
-        if s:
-            s.discard(member)
-            if not s:
-                # optionally remove empty set
-                del self.sets[key]
-
-    def smembers(self, key):
-        return set(self.sets.get(key, set()))
-
-    def expire(self, key, ttl):
-        # ignore TTL in fake
-        return True
-
-    def delete(self, key):
-        self.store.pop(key, None)
-        self.sets.pop(key, None)
-
-'''
-r = FakeRedis()
-db = Database(r,'database/database.db')
-'''
-
-DATABASE = 'database/database.db'
-'''
-def get_db():
-    db = getattr(g,'_database', None)
-    if db is None :
-        db = g._database = sqlite3.connect(DATABASE)
-    return db
-'''
-
+load_dotenv()
 DATABASE= os.getenv('DATABASE')
 
 def get_db():
@@ -81,7 +25,7 @@ def get_db():
 
 def get_projets_by_convention(id_convention):
     c = get_db().cursor()
-    c.execute("SELECT * FROM Projets WHERE convention_id="+str(id_convention))
+    c.execute("SELECT * FROM Projets WHERE convention_id= ?",(id_convention,))
     projets = []
     for projet in c.fetchall():
         projets.append( {"id":projet[0],"convention_id":projet[1],
@@ -93,7 +37,7 @@ def get_projets_by_convention(id_convention):
 
 def get_client(id):
     c = get_db().cursor()
-    c.execute("SELECT * FROM Clients WHERE client_id="+str(id))
+    c.execute("SELECT * FROM Clients WHERE client_id=?", (id,))
     for client in c.fetchall():
         return {"id":client[0],"nom_entreprise":client[1],
                 "contact_nom":client[2],"contact_email":client[3],
@@ -104,7 +48,7 @@ def get_client(id):
 
 def get_convention(id):
     c = get_db().cursor()
-    c.execute("SELECT * FROM Conventions WHERE convention_id="+str(id))
+    c.execute("SELECT * FROM Conventions WHERE convention_id=?",(id,))
     for convention in c.fetchall():
         return {"id":convention[0],"nom":convention[1],
                 "description":convention[2],"date_debut":convention[3],
@@ -122,7 +66,7 @@ def liste_conventions():
 
 def get_utilisateur(id):
     c = get_db().cursor()
-    c.execute("SELECT utilisateur_id,nom,prenom FROM Utilisateurs WHERE utilisateur_id="+str(id))
+    c.execute("SELECT utilisateur_id,nom,prenom FROM Utilisateurs WHERE utilisateur_id=?",(id,))
     for utilisateur in c.fetchall():
         return {"id":utilisateur[0],"nom":utilisateur[1],"prenom":utilisateur[2]}
     return {}
