6f0008d21f85ee04e947d0eb49a7314232eab395
titouan.rose <titouan.rose@telecomnancy.eu>
2026-01-07 17:28:53 +0100

    Enlevé fichiers pour la version release

--- GdP/Gestion_des_risques.md
+++
@@ -1,2 +0,0 @@
-
-Comme le risque peut être évalué par un équivalent monétaire, mais aussi que le budget du projet est de 0€, alors il n'y a aucun risque significatif pour ce projet.
--- database/add_default_project.sql
+++ database/add_default_project.sql
@@ -4,7 +4,7 @@ INSERT INTO Projets(convention_id,
     budget,
     date_debut,
     date_fin,
-    statut, doc_dossier) VALUES (0,'projet test', 'ceci est vraiment un beau test, moi je dis bravo', 1.5, '2025-04-28','2025-04-29', 'Annulé', 'https://example.org');
+    statut, doc_dossier) VALUES (0,'projet test', 'description test', 1.5, '2025-04-28','2025-04-29', 'Annulé', 'https://example.org');
 
 INSERT INTO Jalons(description, est_complete, date_fin, projet_id) VALUES ('signature de la charte de projet', 1, '2025-04-28', 1);
 INSERT INTO Jalons(description, est_complete, date_fin, projet_id) VALUES ('Implémentation de la base de données', 1, '2025-04-29', 1);
